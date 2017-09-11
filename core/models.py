c82d23472c2327c8d14062c89e06fc92abdbc7c6
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-09-11 18:11:15 +0000

    Add app, pip requirements.

---
+++ core/models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Service(models.Model):
+    # name
+    # description
+    # url
+    # date_created
+    pass
+
+class Organization(models.Model):
+    pass
+
+class Agent(models.Model):
+    pass
