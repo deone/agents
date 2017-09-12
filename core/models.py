516c6b0625d13a543f7352e2ce379eadadfbd707
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-09-12 22:45:44 +0000

    Add is_active field to Service, Organization and Agent.

--- core/models.py
+++ core/models.py
@@ -5,6 +5,9 @@ from django.db import models
 
 from agent.models import Entity, NeoAgent
 
+# Don't delete Organization and Agent instances. De-activate them instead.
+# Update instances on services when either of these actions happen.
+
 class Service(Entity):
     description = models.CharField(max_length=50)
     url = models.URLField()
