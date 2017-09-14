162c522a94dfb398e600a32cf1cccd1a587afe1b
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-09-14 15:41:21 +0000

    Display phone number model form on same page as agent model form.

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,13 @@ from django.contrib import admin
 
 from models import *
 
-admin.site.register(Agent)
+class PhoneNumberInline(admin.StackedInline):
+    model = PhoneNumber
+
+class AgentAdmin(admin.ModelAdmin):
+    inlines = (PhoneNumberInline, )
+
 admin.site.register(Service)
 admin.site.register(Organization)
 admin.site.register(PhoneNumber)
+admin.site.register(Agent, AgentAdmin)
