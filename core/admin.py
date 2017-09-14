867aa25540886a7d7890266c2e0e0d609241307a
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-09-14 15:30:27 +0000

    Enable PhoneNumber in admin.

--- core/admin.py
+++ core/admin.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 
 from models import *
 
+admin.site.register(Agent)
 admin.site.register(Service)
 admin.site.register(Organization)
-admin.site.register(Agent)
+admin.site.register(PhoneNumber)
